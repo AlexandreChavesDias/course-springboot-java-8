85f5b6bdbd259144d9338285ac972f991047b696
AlexandreChavesDias <alexandrechavesdias@hotmail.com>
2020-06-09 17:15:45 -0300

    Exception handling - delete

--- src/main/java/com/alexandredias/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/alexandredias/course/resources/exceptions/ResourceExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.alexandredias.course.services.exceptions.DatabaseException;
 import com.alexandredias.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Database erros";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/alexandredias/course/services/UserService.java
+++ src/main/java/com/alexandredias/course/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.alexandredias.course.entities.User;
 import com.alexandredias.course.repositories.UserRepository;
-import com.alexandredias.course.resources.exceptions.ResourceExceptionHandler;
+import com.alexandredias.course.services.exceptions.DatabaseException;
 import com.alexandredias.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -26,17 +28,23 @@ public class UserService {
 	}
 	// Operações DB
 
-	           // insere
+	// insere
 	public User insert(User obj) {
 		return repository.save(obj); // obj salvo
 	}
 
-               //delete
+	// delete
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
-         	// atualizar
+	// atualizar
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);// prepara o obj monitorado para mexer e depois alterar no DB
 		updateData(entity, obj);
